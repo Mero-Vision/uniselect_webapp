de479f17d0d11775e186534c59ac985427ec550c
Hancie123 <hanciewanemphago@gmail.com>
2025-02-11 18:30:00 +0545

    student home added

--- resources/views/students/layouts/header.blade.php
+++ resources/views/students/layouts/header.blade.php
@@ -1,5 +1,4 @@
-<link rel="shortcut icon" href="https://product.geniusocean.com/genius-wallet/assets/images/5480339181644482400.png">
-
+<link rel="shortcut icon" href="{{ url('assets/login/img/logo.png') }}">
 <link rel="stylesheet" href="{{url('assets/students/css/font-awsome.min.css')}}">
 <link href="{{url('assets/students/css/tabler.min.css')}}" rel="stylesheet"/>
 <link href="{{url('assets/students/css/tabler-flags.min.css')}}" rel="stylesheet"/>
