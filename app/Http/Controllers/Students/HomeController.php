f2c60913abaa9fa06e435362b4f4856d3ba037cc
Hancie123 <hanciewanemphago@gmail.com>
2025-03-29 15:53:32 +0545

    home controller updated

--- app/Http/Controllers/Students/HomeController.php
+++ app/Http/Controllers/Students/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
         if (!$student || !$student->testScores){
             $warningMessage = 'Test scores not found. Please update your test scores to get course recommendations.';
         }
-        $applications=Application::with('university','course')->latest()->get();
+        $applications=Application::where('student_id',Auth::user()->id)->with('university','course')->latest()->get();
         
         return view('students.home.home',compact('totalUniversity','totalCourses','warningMessage','applications','totalApplications'));
     }
